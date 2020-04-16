6316b546ab70d2e5001b4831911cca2dcc58463f
lindoro <github.com/lindoro7>
2020-04-16 16:44:43 +0300

    Add firebase to project

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ import messagePlugin from '@/utils/message.plugin'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min.js'
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
 Vue.config.productionTip = false
 
 Vue.use(messagePlugin)
@@ -15,8 +19,28 @@ Vue.use(Vuelidate)
 
 Vue.filter('date', dateFilter)
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+firebase.initializeApp({
+  apiKey: "AIzaSyCp0aUpfJ8BACR06o9P5MjzLBKvfNTxAJE",
+  authDomain: "vue-crm-testing.firebaseapp.com",
+  databaseURL: "https://vue-crm-testing.firebaseio.com",
+  projectId: "vue-crm-testing",
+  storageBucket: "vue-crm-testing.appspot.com",
+  messagingSenderId: "607118329698",
+  appId: "1:607118329698:web:028c80e4975780c6208e3b",
+  measurementId: "G-PDXTH49WGJ"
+})
+
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+  if(!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+  
+})
+
+
